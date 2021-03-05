ec2c2c22d16a18119b86f6402e7d58de9e1469c6
SerikM <tyrkamotchka@mail.ru>
2021-03-05 12:05:50 +1100

    updated entityframework libraries

---
+++ api/src/data/Migrations/20210305010326_initial.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PROJECTS",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    ClientName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    JobNo = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PROJECTS", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "REPORTS",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_REPORTS", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Month",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Revenue = table.Column<double>(type: "float", nullable: false),
+                    ProjectId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Month", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Month_PROJECTS_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "PROJECTS",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Month_ProjectId",
+                table: "Month",
+                column: "ProjectId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Month");
+
+            migrationBuilder.DropTable(
+                name: "REPORTS");
+
+            migrationBuilder.DropTable(
+                name: "PROJECTS");
+        }
+    }
+}
